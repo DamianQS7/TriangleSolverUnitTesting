665371688155e2faec14d1428e7d8e6c13510e4d
DamianQS7 <102097286+DamianQS7@users.noreply.github.com>
2023-02-07 13:52:19 -0500

    Added Unit Tests for Equilateral Triangles

--- TriangleSolver/TriangleSolverTestLibrary/TriangleSolverTest.cs
+++ TriangleSolver/TriangleSolverTestLibrary/TriangleSolverTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using TriangleSolver;
 
 
 namespace TriangleSolverTestLibrary
@@ -12,6 +13,205 @@ namespace TriangleSolverTestLibrary
     public class TriangleSolverTest
     {
         [Test]
-        public void Test() { }
+        public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        {
+            // Arrange
+            Triangle triangle = new Triangle();
+            int firstSide = 60;
+            int secondSide = 60;
+            int thirdSide = 60;
+
+            string expected = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
+
+            // Act
+            string actual = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.IsTrue(expected.Equals(actual));
+        }
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    Triangle triangle = new Triangle();
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+        //    string actual = triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void AnalyzeTriangle_Input60_60_60_OutputEquilateralTriangle()
+        //{
+        //    // Arrange
+        //    int firstSide = 60;
+        //    int secondSide = 60;
+        //    int thirdSide = 60;
+
+        //    // Act
+
+        //    // Assert
+        //}
     }
 }
